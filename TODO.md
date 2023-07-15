108cb4a0426b2722a92a6eec959c2407713f2322
Smoke <40275383+creizlein@users.noreply.github.com>
2023-07-15 02:15:18 -0300

    Implemented a new MarkerLabel to highlight changes.

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 These features have to be implemented because they are not part of old VB.net project
 
-* Performance numbers should highlight when they change, ideally with +/- sign and colour, or draw a small box on the right edge.
 * DarkMode - Has to be custom to set colors properly.
 * perfChart should be able to split to show 1 graph per value.
 * Parent PID should be displayed, and possible grouped by and/or hide similars.
---
+++ src/Controls/sMkMarkerLabel.cs
@@ -0,0 +1,40 @@
+﻿namespace sMkTaskManager.Controls;
+
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+public class sMkMarkerLabel : Label {
+    private bool _drawIcon = false;
+    private readonly System.Timers.Timer tmrTimeout = new() { Enabled = false, Interval = 3000, AutoReset = false };
+
+    public sMkMarkerLabel() : base() {
+        ShowMark = true;
+        MarkColor = Color.FromKnownColor(KnownColor.Highlight);
+        Paint += OnPaint;
+        tmrTimeout.Elapsed += OnTimerElapsed;
+    }
+    public bool ShowMark { get; set; }
+    public Color MarkColor { get; set; }
+    public new string Text {
+        get { return base.Text; }
+        set {
+            if (value.Equals(Text)) {
+                if (_drawIcon) { _drawIcon = false; tmrTimeout.Stop(); Invalidate(); }
+            } else {
+                _drawIcon = true;
+                base.Text = value;
+                tmrTimeout.Stop(); tmrTimeout.Start();
+            }
+        }
+    }
+
+    private void OnPaint(object? sender, PaintEventArgs e) {
+        if (_drawIcon && Visible && ShowMark && Width > 2 && Height > 2) {
+            e.Graphics.DrawLine(new Pen(MarkColor, 2), Width - 1, 1, Width - 1, Height - 1);
+        }
+    }
+    private void OnTimerElapsed(object? sender, EventArgs e) {
+        _drawIcon = false;
+        Invalidate();
+        tmrTimeout.Stop();
+    }
+
+}
